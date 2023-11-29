ec68b834b19fd7a1b3ed77c04983cd2e715776bb
Pascal <pascalokereke18@gmail.com>
2023-11-29 23:03:11 +0100

    shopify

--- shopify.js
+++ shopify.js
@@ -1,4 +1,7 @@
 function app() {
+
+const aHidden = 'hidden';
+const MARKED_AS_DONE = 'checkbox-done';
 // the cancel button
 // users that is not interested
 
@@ -9,16 +12,89 @@ cancelBtn.addEventListener('click', () => {
   planNotification.style.display = 'none';
 })
 
-    // the button toggle
-document.addEventListener('DOMContentLoader', function() {
-        const card = document.getElementById('#item-checkbox');
-        const toggleButton = document.getElementById('#button-guide') 
-    
-        toggleButton.addEventListener('click',  function() {
-            card.classList.toggle('closed');
-        })
-    
-    })
+// onclick, hide the empty checkbox 
+// show the spinner
+// 3 seconds later, show the check icon
+// get the button and all the icons
+
+const checkboxButton = document.querySelector("#item-checkbox")
+
+//get the button 
+// not completed icon and loading spinner icon
+const notCompletedIcon = checkboxButton.querySelector('#not-completed-icon');
+
+const checkboxButtonStatus = document.querySelector('#item-status');
+
+const loadingSpinnerIcon = checkboxButton.querySelector('#spinner-icon');
+
+const completedIcon = checkboxButton.querySelector('#completed-icon');
+console.log(checkboxButton, 
+    notCompletedIcon, 
+    loadingSpinnerIcon, 
+    completedIcon);
 }
 
-app()
+
+
+function handleMarkAsDone() {
+    notCompletedIcon.classList.add(aHidden);
+
+    loadingSpinnerIcon.classList.remove(aHidden);
+
+    checkboxButton.ariaLabel = 'Loading, Please wait...'
+
+    setTimeout(() => {
+        loadingSpinnerIcon.classList.add(aHidden);
+        completedIcon.classList.remove(aHidden);
+        
+        checkboxButton.ariaLabel = checkboxButton.ariaLabel.replace('as done',
+         "as not done");
+
+        checkboxButton.ariaLabel ='Successfully marked the box as done'
+
+        checkboxButton.classList.add(MARKED_AS_DONE);
+    }, 3000);
+
+}
+
+function handleMarkAsNotDone() {
+   completedIcon.classList.add(aHidden);
+   loadingSpinnerIcon.classList.remove(aHidden);
+   
+   checkboxButton.ariaLabel = 
+   "Loading, Pease wait..."
+
+   setTimeout(() => {
+      loadingSpinnerIcon.classList.add(aHidden);
+
+      notCompletedIcon.classList.remove(aHidden);
+      
+      checkboxButton.ariaLabel = checkboxButton.ariaLabel.replace('as done',
+      "as not done");
+
+      checkboxButton.ariaLabel = "Successfully marked your box as not done."
+
+   }, 3000);
+
+}
+
+
+//on click, hide the empty checkbox
+function itemCheckDoneOrNotDone() {
+    const markedAAsDone = checkboxButton.classList.contains(MARKED_AS_DONE);
+
+    if(markedAAsDone) {
+        itemCheckDoneOrNotDone()
+    }
+    else {
+        handleMarkAsDone();
+    }
+}
+
+checkboxButton.addEventListener('click', itemCheckDoneOrNotDone);
+
+// 3 seconds later show the check icon
+
+
+
+app();
